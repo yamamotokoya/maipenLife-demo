94c0ac5bfcb437844b04cf87be2d5e7bc6ffa0c8
Koya Yamamoto <koyayamamoto0@gmail.com>
2023-12-01 16:37:35 +0900

    changed routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'streamers#new'
   resources :streamers, only: [:index, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
