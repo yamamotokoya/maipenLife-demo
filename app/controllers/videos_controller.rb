79a06256f48eef4821f9cf07907d24c9fda2ddce
Koya Yamamoto <koyayamamoto0@gmail.com>
2023-12-25 08:49:10 +0900

    add video destroy action

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,4 +1,6 @@
 class VideosController < ApplicationController
+  before_action :set_video, only: [:show, :edit, :update, :destroy]
+
   def index
     @videos = Video.all 
     render json: ActiveModel::Serializer::CollectionSerializer.new(
@@ -19,15 +21,27 @@ class VideosController < ApplicationController
   end
 
   def show 
-    @video = Video.find(params[:id])
   end
 
   def edit 
-    @video = Video.find(params[:id])
+  end
+
+  def update 
+    @video.update(video_params)
+    redirect_to edit_streamer_path(@video.streamer_id)
+  end
+
+  def destroy 
+    @video.delete 
+    redirect_to request.referer
   end
 
   private 
     def video_params
       params.require(:video).permit(:video_url, :content, :streamer_id)
+    end
+
+    def set_video 
+      @video = Video.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'streamers#new'
   resources :streamers, only: [:index, :new, :create, :edit, :update, :destroy]
   get 'all_streamers', to: 'streamers#all_streamers'
-  resources :videos, only: [:index, :create, :show, :edit, :update]
+  resources :videos, only: [:index, :create, :show, :edit, :update, :destroy]
   get 'new/:id', to: "videos#new", as: 'new_video'
 end
